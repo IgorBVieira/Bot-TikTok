abcf864f94cca27c89b2660230d92d9205d143b8
igor.vieira <igor.vieira@itmil.com.br>
2022-11-23 13:24:10 -0300

    Inicio projeto DEVAPRENDER

---
+++ Projeto TikTok/app.py
@@ -0,0 +1,15 @@
+# Passos manuais em sequência, depois tornar cada passo em um codigo
+# Quais são os passos manuais
+
+# #clicar em login
+# clicar em logar com email 
+# Clicar em logar com email e senha
+# Clicar em login
+# Navegar até a pagina (colocar link)
+# cliccar na postagem mais recente
+# Verificar se video já foi curtido
+# Se foi curtido passar para próximo video
+# Se não foi curtido, curtir este video e passar para próximo video
+# Repetir por quantas vezes quiser
+
+# https://www.youtube.com/watch?v=AFffT1qmuGs
